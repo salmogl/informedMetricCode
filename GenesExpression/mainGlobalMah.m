4336bde02a8af1d7f78a98c7e26036148354ba8a
salmogl <lahavalmog@gmail.com>
2018-06-25 13:12:04 +0300

    gene expression - global Mahalanobis

--- GenesExpression/mainGlobalMah.m
+++ GenesExpression/mainGlobalMah.m
@@ -1,16 +1,12 @@
 clear all;
 close all hidden;
 clc; 
+
 dataSet             = 'MSK';
 loadPath            = addPathLoad(dataSet);
 load(loadPath);
-
-plotFlag            = true;
-if (plotFlag)
-    set(0,'DefaultFigureVisible','on')
-else
-    set(0,'DefaultFigureVisible','off')
-end
+fSize               = 15;
+set(0,'DefaultFigureVisible','off')
 
 data                = dataStruct.genesData;
 month               = dataStruct.month;
@@ -20,32 +16,7 @@ vitalStat           = dataStruct.vitalStat;
 data                = data(indMaxStd(2:201),:);
 [M,N]               = size(data);
 rank                = 5;
-kClusters           = 21;%10:1:30;
-
-for kk = 1:length(kClusters)
-
-class               = kmeans(data,kClusters(kk),'MaxIter',1e6,'OnlinePhase','on','replicates',1e2);
-[~,indSort]         = sort(class);
-
-figure;
-fSize = 15;
-HMObject = HeatMap(data(indSort,:));
-addYLabel(HMObject, 'Genes','FontSize', fSize)
-addXLabel(HMObject, 'Subjects','FontSize', fSize)
-
-figure;
-HMObject = HeatMap(data);
-addYLabel(HMObject, 'Genes','FontSize', fSize)
-addXLabel(HMObject, 'Subjects','FontSize', fSize)
-
-
-
-[~,S0,Upca]         = svd(cov(data'));
-Upca                = Upca(:,1:rank);
-S                   = S0(1:rank,1:rank);
-covPca              = Upca*S*Upca';
-covSam              = cov(data');
-
+kClusters           = 21; %10:1:40;
 
 params.rank         = rank;
 params.mu           = 1e-1;
@@ -54,81 +25,68 @@ params.niter        = 500;
 params.stepOpt      = 'constStepLength';
 params.debugMode    = true;
 params.threshDiff   = 5e-5;
-Uinf                = getPdSubProj(data,class,params);
-covInf              = Uinf*S*Uinf';
 
-%%
-figure;
-imagesc(covPca(indSort,indSort));
-% title('PCA')
+nIter               = 1;
+pPcaAllK            = zeros(1,length(kClusters));
+pInfAllK            = zeros(1,length(kClusters));
+pVarInfAllK         = zeros(1,length(kClusters));
 
-figure;
-imagesc(covSam(indSort,indSort))
-% title('Sample Covariance')
+for kk = 1:length(kClusters)
 
-figure;
-imagesc(covInf(indSort,indSort))
-% title('Informed PCA')
+    pInfAllp            = zeros(1,nIter);
+    pPcaAllp            = zeros(1,nIter);
 
-%%
-dataCentered = bsxfun(@minus,data,mean(data,2));
-Vpca         = (Upca*S^(-1/2))'*dataCentered;
-Vinf         = (Uinf*S^(-1/2))'*dataCentered;
-figure;
-subplot(1,2,1)
-plot3dData(Vpca(1:3,:),vitalStat,'Embedding Colored By Vital Status (Dead) (PCA)')
+    for ii = 1 : nIter
 
-subplot(1,2,2)
-plot3dData(Vinf(1:3,:),vitalStat,'Embedding Colored By Vital Status (Dead) (Informed)')
+        class               = kmeans(data,kClusters(kk),'MaxIter',1e6,'OnlinePhase','on','replicates',1e5);
+        [~,indSort]         = sort(class);
 
-%%
-% groupsPca           = ones(N,1);
-% Vpca1               = Vpca(1,:)*Vpca(1,1);
-% groupsPca(Vpca1 > 0)= 2;
-groupsPca           = kmeans(Vpca',2,'MaxIter',1e6,'OnlinePhase','on','replicates',500);
-%%
-[x1_pca,x2_pca]     = getRiskGroups(month,vitalStat,groupsPca);
-figure;
-pPca                = logrank(x2_pca,x1_pca);
-fSize               = 15;
-title('')
-xlabel('Time [Month]','Interpreter','Latex','FontSize',fSize)
-ylabel('Estimated survival function','Interpreter','Latex','FontSize',fSize)
-le                  = legend('Group 1','Censord','Group 2')
-set(le,'Interpreter','Latex','FontSize',fSize-2)
-xlim([0 60])
+        [~,S0,Upca]         = svd(cov(data'));
+        Upca                = Upca(:,1:rank);
+        S                   = S0(1:rank,1:rank);
+        covPca              = Upca*S*Upca';
+        covSam              = cov(data');
+        Uinf                = getPdSubProj(data,class,params);
+        covInf              = Uinf*S*Uinf';
 
-%%
-% groupsInf           = ones(N,1);
-% Vinf1               = Vinf(1,:)*Vinf(1,1);
-% groupsInf(Vinf1 > 0)= 2;tsneVc
+        %%
+        dataCentered        = bsxfun(@minus,data,mean(data,2));
+        Vpca                = (Upca*S^(-1/2))'*dataCentered;
+        Vinf                = (Uinf*S^(-1/2))'*dataCentered;
 
-groupsInf           = kmeans(Vinf',2,'MaxIter',1e6,'OnlinePhase','on','replicates',500);
-%%
-[x1_inf,x2_inf]     = getRiskGroups(month,vitalStat,groupsInf);
-figure;
-pInf                = logrank(x2_inf,x1_inf);
-fSize               = 15;
-title('')
-xlabel('Time [Month]','Interpreter','Latex','FontSize',fSize)
-ylabel('Estimated survival function','Interpreter','Latex','FontSize',fSize)
-le                  = legend('Group 1','Censord','Group 2')
-set(le,'Interpreter','Latex','FontSize',fSize-2)
-xlim([0 60])
+        %%
+        groupsPca           = kmeans(Vpca',2,'MaxIter',1e6,'OnlinePhase','on','replicates',500);
+        [x1_pca,x2_pca]     = getRiskGroups(month,vitalStat,groupsPca);
+        pPcaAllp(ii)        = logrank(x2_pca,x1_pca);
 
-pPcaAllK(kk) = pPca;
-pInfAllK(kk) = pInf;
+        groupsInf           = kmeans(Vinf',2,'MaxIter',1e6,'OnlinePhase','on','replicates',500);
+        [x1_inf,x2_inf]     = getRiskGroups(month,vitalStat,groupsInf);
+        pInfAllp(ii)        = logrank(x2_inf,x1_inf);
+
+    end
+
+    pPcaAllK(kk)         = mean(pPcaAllp);
+    pInfAllK(kk)         = mean(pInfAllp);
+    pVarInfAllK(kk)      = var(pInfAllp);
 
 end
-%%
-% no_dims         = 3;
-% initial_dims    = rank;
-% perplexity      = 20;
-% clustLabels     = vitalStat;
-% figure;
-% tsneVc          = tsne_inf(data', clustLabels, no_dims, initial_dims, perplexity);
-% tsneRaw         = tsne(Vinf', clustLabels, no_dims, initial_dims, perplexity);
-%%
+
+%% Figures
+set(0,'DefaultFigureVisible','on')
+
+% Data
+% genes are ordered according to their variance
+HMObject = HeatMap(data);
+addYLabel(HMObject, 'Genes','FontSize', fSize)
+addXLabel(HMObject, 'Subjects','FontSize', fSize)
+
+% genes are ordered according to clusters obtained by k-means
+HMObject = HeatMap(data(indSort,:));
+addYLabel(HMObject, 'Genes','FontSize', fSize)
+addXLabel(HMObject, 'Subjects','FontSize', fSize)
+
+
+% Eigenvalues decay
 dataSet             = 'CANDF';
 loadPath            = addPathLoad(dataSet);
 load(loadPath)
@@ -136,9 +94,8 @@ dataCANDF           = dataStruct.genesData;
 [val,indMaxStd]     = sort(std(dataCANDF,[],2),'descend');
 dataCANDF           = dataCANDF(indMaxStd(2:201),:);
 [~,S0CANDF,~]       = svd(cov(dataCANDF'));
-%%
-eigsMSK         = sum(S0)/max(S0(:));
-eigsCAN         = sum(S0CANDF)/max(S0CANDF(:));
+eigsMSK             = sum(S0)/max(S0(:));
+eigsCAN             = sum(S0CANDF)/max(S0CANDF(:));
 
 figure;
 plot(eigsMSK)
@@ -152,35 +109,47 @@ plot(ones(1,length(eigsMSK))*5,linspace(0,eigsMSK(5),length(eigsMSK)),'b--')
 xlim([1 20])
 ylabel('Normalized eigenvalues ($\lambda_i$)','Interpreter','Latex','FontSize',fSize)
 xlabel('$i$','Interpreter','Latex','FontSize',fSize)
-%%
-fSize = 15;
-load('../MatFiles/pValVsK_10_1_40_var.mat')
-% pPcaAllK_70_5_100   = pPcaAllK;
-% pInfAllK_70_5_100   = pInfAllK;
-% kClusters_70_5_100  = kClusters;
-% load('../MatFiles/pValVsK_1.mat')
-% pPcaAllK            = [pPcaAllK pPcaAllK_70_5_100];
-% pInfAllK            = [pInfAllK pInfAllK_70_5_100];
-% kClusters           = [kClusters kClusters_70_5_100];
-std0  = pVarInfAllK;
-kClusters = kClusters(1:length(pPcaAllK))
+
+% Kaplan-Meier survival plots
 figure;
-% plot(x,y,'r.-');
-h(1) = plot(kClusters,pPcaAllK,'r.-')
-hold on
-h(2) = patch([kClusters fliplr(kClusters)],[pInfAllK+std0 fliplr(pInfAllK-std0)],0.8*[1 1 1]);
-h(3) = plot(kClusters,pInfAllK,'b.-')
+pPca                = logrank(x2_pca,x1_pca);
+title('')
+xlabel('Time [Month]','Interpreter','Latex','FontSize',fSize)
+ylabel('Estimated survival function','Interpreter','Latex','FontSize',fSize)
+le                  = legend('Group 1','Censord','Group 2');
+set(le,'Interpreter','Latex','FontSize',fSize-2)
+xlim([0 60])
 
-le = legend(h([1 3]),'$\hat{p}$','$\tilde{p}$')
+figure;
+pInf                = logrank(x2_inf,x1_inf);
+title('')
+xlabel('Time [Month]','Interpreter','Latex','FontSize',fSize)
+ylabel('Estimated survival function','Interpreter','Latex','FontSize',fSize)
+le                  = legend('Group 1','Censord','Group 2');
+set(le,'Interpreter','Latex','FontSize',fSize-2)
+xlim([0 60])
 
-set(le,'Interpreter','Latex','FontSize',fSize-3)
 
+% P-Value Vs. number of clusters
 figure;
 plot(kClusters,log(pPcaAllK));
 hold on
 plot(kClusters,log(pInfAllK));
-ylabel('p-Value [dB]','fontsize',15)
-xlabel('K [Clusters]','fontsize',15)
+ylabel('p-Value [dB]','fontsize',fSize)
+xlabel('K [Clusters]','fontsize',fSize)
 xlim([10 30])
 le = legend('$\hat{p}$','$\tilde{p}$');
-set(le,'Interpreter','Latex','FontSize',15)
+set(le,'Interpreter','Latex','FontSize',fSize)
+
+% Comparison of covariance matrices
+figure;
+imagesc(covSam(indSort,indSort))
+% title('Sample Covariance')
+
+figure;
+imagesc(covPca(indSort,indSort));
+% title('PCA')
+
+figure;
+imagesc(covInf(indSort,indSort))
+% title('Informed PCA')
--- plotCovariance.m
+++ plotCovariance.m
@@ -6,7 +6,6 @@ coorZoom    = [750 900];
 as          = -46; % View parameters
 el          = 11; 
 
-
 figure;
 mesh(1:sizeRows,1:sizeRows,covariance);
 
@@ -14,18 +13,12 @@ zlim([-2 30])
 xlim([200 sizeRows])
 ylim([200 sizeRows])
 
-% xlim([1 sizeRows]);ylim([1 sizeRows])
-
 xlabel('$i$','FontSize',fontS)
 ylabel('$j$','FontSize',fontS)
 zlabel(title,'FontSize',fontS)
-% create a new pair of axes inside current figure
 axes('position',[.13 .5 .3 .35])
-box on % put box around new pair of axes
-mesh(1:sizeRows,1:sizeRows,covariance) % plot on new axes
-% view(2);
-% axis tight
-
+box on
+mesh(1:sizeRows,1:sizeRows,covariance)
 
 set(gcf,'Renderer','Zbuffer')
 xlim(coorZoom)
@@ -33,12 +26,6 @@ ylim(coorZoom)
 zlim([11 28])
 view([as el])
 
-% set(gca,'xtick',[])
-% set(gca,'xticklabel',[])
-% set(gca,'ytick',[])
-% set(gca,'yticklabel',[])
-% set(gca,'ztick',[])
-% set(gca,'zticklabel',[])
 set(gca,'color','none')
 axis off
 
